7fa930dc8deedf5d947cd017d21ec002cf897306
Josh <josh.walters@shaw.ca>
2022-07-07 14:00:17 -0600

    exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num, num2, num3, num4) {
+    let index;
+    for(let i =0; i<array.length;i++){
+        if(array[i] === num || array[i] === num2 || array[i] === num3 || array[i] === num4){
+            array.splice(i,1);
+            i=-1
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
